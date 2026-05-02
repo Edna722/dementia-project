1e53dca54720ca89750f19c8679205c1d3eac16c
Edna <wanjikuedna3@gmail.com>
2026-05-02 15:56:27 +0800

    Statistical Analysis

--- dementia project code.R
+++ dementia project code.R
@@ -41,13 +41,63 @@ sapply (data, function(x) length(unique(x)))
 # Impute Categorical data 
 data <- data %>%
   mutate(
-    Education_ID = as.factor(Education_ID),
-    Mobility = as.factor(Mobility),
-    MMSE_class = as.factor(MMSE_class),
-    Hyperlipidaemia = as.factor(Hyperlipidaemia)
+    Education_ID = as.factor (Education_ID),
+    Mobility = as.factor (Mobility),
+    MMSE_class = as.factor (MMSE_class),
+    Hyperlipidaemia = as.factor (Hyperlipidaemia)
   )
 
 # In depth statistics 
+data %>%
+  select(where (is.numeric)) %>%
+  summary()
+
+# In-depth stats summary for age, body_weight, body_height, waist
+data %>%
+  summarise(
+    mean_age = mean (Age, na.rm = TRUE),
+    Median_age = median (Age,na.rm = TRUE),
+    sd_age = sd (Age, na.rm = TRUE),
+    min_age = min (Age, na.rm = TRUE),
+    max_age = max (Age, na.rm = TRUE)
+  )
+
+# Numeric variables statistics 
+key_vars <-data %>%
+  select (Age, body_weight, body_height, waist)
+
+# Descriptive Statistics 
+dsesc_stats <- key_vars%>%
+  summarise (across(
+    everything (),
+    list(
+      mean = ~ mean (., na.rm = TRUE),
+      median = ~ median (.,na.rm = TRUE),
+      sd = ~ sd (.,na.rm = TRUE),
+      min = ~ min (., na.rm = TRUE),
+      max = ~ max (., na.rm = TRUE),
+      Q1 = ~ quantile (., 0.25,  na.rm = TRUE),
+      Q3 = ~ quantile (., 0.75, na.rm = TRUE)
+    ),
+    
+    .names = "{.col}_{.fn}"
+  ))
+
+desc_stats
+
+# Histogram Analysis 
+plot_histogram <- function (var){
+  ggplot(data, aes (x = .data[[var]])) +
+    geom_histogram(bins = 30, fill = "steelblue", color = "black") +
+    labs (title = paste ("Distribution" , var), x = var, y = "Count") +
+    theme_minimal()
+}
+
+# Plotting
+plot_histogram("Age")
+plot_histogram("body_weight")
+plot_histogram("body_height")
+plot_histogram("waist")
 
 
 # Data cleaning 
