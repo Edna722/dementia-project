facbe46cb94b1b3e25d7ea1500b8e2189ea1e501
Edna <wanjikuedna3@gmail.com>
2026-05-19 17:00:45 +0800

    Creating dummy variables

--- dementia project code.R
+++ dementia project code.R
@@ -9,6 +9,7 @@ install.packages("tidyverse")
 install.packages("naniar")
 install.packages("missMethods")
 install.packages("corrplot")
+install.packages("caret")
 
 
 
@@ -23,10 +24,11 @@ library(heatmaply)
 library(tidyverse)
 library(missMethods)
 library(corrplot)
+library(caret)
 
 ## Data Munging
 # Load the dataset 
-data <- read_excel("C:/Users/wanji/Desktop/ICT 583/ICT583 s1 2026 dataset.xlsx")
+data =  read_excel("C:/Users/wanji/Desktop/ICT 583/ICT583 s1 2026 dataset.xlsx")
 
 str (data)
 
@@ -54,6 +56,7 @@ data %>%
   summary()
 
 # In-depth stats summary for age, body_weight, body_height, waist
+
 data %>%
   summarise(
     mean_age = mean (Age, na.rm = TRUE),
@@ -64,6 +67,7 @@ data %>%
   )
 
 # Numeric variables statistics 
+
 key_vars <-data %>%
   select (Age, body_weight, body_height, waist)
 
@@ -111,12 +115,15 @@ missing_percent
 # Handling the missing values for Numerical and Categorical variables 
 
 # Impute for numerical variables
+
 data <- data %>%
   mutate (across(
     where (is.numeric),
     ~ ifelse (is.na (.), median (., na.rm = TRUE),.)
   ))
+
 # Function for categorical method imputation 
+
 get_mode <- function(x) {
   x <- x [!is.na(x)]
   ux <- unique(x)
@@ -124,6 +131,7 @@ get_mode <- function(x) {
 }
 
 # Impute for Categorical variables 
+
 data <- data %>%
   mutate(across(
     where(is.factor),
@@ -132,7 +140,9 @@ data <- data %>%
       factor(ifelse(is.na(.), as.character(mode_val), as.character(.)), levels = levels(.))
     }
   ))
+
 # Checking if there are missing values 
+
 data %>%
   
   is.na () %>%
@@ -140,6 +150,7 @@ data %>%
   colSums ()
 
 # Histogram Analysis 
+
 plot_histogram <- function (var){
   ggplot(data, aes (x = .data[[var]])) +
     geom_histogram(bins = 30, fill = "steelblue", color = "black") +
@@ -148,18 +159,22 @@ plot_histogram <- function (var){
 }
 
 # Plot for Age
+
 plot_histogram("Age")
 
-#Plot for body_weight
+# Plot for body_weight
+
 plot_histogram("body_weight")
 
 # plot for height
+
 plot_histogram("body_height")
 
 # Plot for waist
 plot_histogram("waist")
 
 # Checking for Outliers 
+
 plot_boxplot <- function(var){
   ggplot(data, aes(y = .data[[var]])) +
            geom_boxplot(fill = "steelblue") +
@@ -168,23 +183,27 @@ plot_boxplot <- function(var){
 }
 
 #   Plot for Age 
+
 plot_boxplot("Age")
 
 # Plot for body_weight
 plot_boxplot("body_weight")
 
-#Plot for body_height
+# Plot for body_height
+
 plot_boxplot("body_height")
 
 # Plot for waist
 plot_boxplot("waist")
 
 # Checking for skewness
+
 skewness_values <- key_vars %>%
   summarise(across (everything(), ~ skewness (., na.rm = TRUE)))
 skewness_values
 
 # Correlation Matrix
+
 cor_matrix <- data %>%
   select ( where ( is.numeric)) %>%
   cor()
@@ -192,13 +211,15 @@ cor_matrix <- data %>%
 cor_matrix
 
 # Visualization for the correlation Matrix plot
+
 corrplot(cor_matrix, method = "color", tl.cex = 0.8)
 
 
 # Check for outliers 
+
 detect_outliers <- function(x) {
   Q1 <- quantile(x,0.25, na.rm = TRUE)
-  Q3 <- quantile(x, 0.25, na.rm = TRUE)
+  Q3 <- quantile(x, 0.75, na.rm = TRUE)
   IQR_val <- Q3 - Q1
   
   lower <- Q1 - 1.5 * IQR_val
@@ -210,11 +231,60 @@ detect_outliers <- function(x) {
   return(x)
   
 }
+
 data <- data %>%
-  mutate(across(c(Age, body_weight, body_weight, waist), cap_outliers))
-
-# 
+  mutate(across(c(Age, body_weight, waist), detect_outliers))
 
 
+# Check Target Distribution
+
+table (data$MMSE_class)
+
+prop.table(table(data$MMSE_class)) * 100
+
+# Feature Analysis
+
+# Numerical vs Target
+ggplot (data, aes (x = MMSE_class, y = Age)) +
   
+  geom_boxplot (fill = "steelblue") +
+  
+  theme_minimal()
+
+ggplot (data, aes (x = MMSE_class, y = body_weight))+
+  
+  geom_boxplot(fill = "orange") +
+  
+  theme_minimal()
+
+# Categorical vs Target Variables 
+
+table (data $ Mobility, data$MMSE_class)
+
+prop.table (table (data$Mobility, data$MMSE_class), 2)
+
+# Remove unwanted variable 
+
+data <- data %>%
+  select (-ID)
+
+# Define Features and Target
+x <- data %>%
+  select (-MMSE_class)
+
+y <- data $MMSE_class
+
+# Encode Categorical Variables 
+
+# Dummy Encoding 
+
+dummies <- dummyVars(MMSE_class ~ ., data = data)
+
+x <- predict(dummies, newdata = data)
+
+x <- as.data.frame(x)
+
+
+
+
   
